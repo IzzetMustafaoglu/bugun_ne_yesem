78e71f7e5ae088603f5149853298d04789e991f4
IzzetMustafaoglu <izzetmustafaoglu00@gmail.com>
2023-08-27 09:29:30 +0300

    TextButton Eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -23,24 +23,44 @@ class BenimUyg extends StatelessWidget {
     );
   }
 }
+
 class YemekSayfasi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
         children: <Widget>[
-          Expanded(child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Image.asset("assets/corba_1.jpg"),
-          )),  // Expanded: Ekrana sığması için kullanılır
-          Expanded(child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Image.asset("assets/yemek_1.jpg"),
-          )),  // flex: Ekran hangsini kaç pay alacağını ayarlar
-          Expanded(child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Image.asset("assets/tatli_1.jpg"),
-          )),  // Expanded(flex: 2,child: Image.asset("assets/yemek_1.jpg")),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextButton(
+                  onPressed: () {
+                    print(
+                        "Çorba Butonu Tıklandı"); // Her tıklandığında Consola Merhaba yazsın
+                  },
+                  child: Image.asset("assets/corba_1.jpg")),
+            ),
+          ), // Expanded: Ekrana sığması için kullanılır
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextButton(
+                  onPressed: () {
+                    print("Yemek Butonu Tıklandı");
+                  },
+                  child: Image.asset("assets/yemek_1.jpg")),
+            ),
+          ), // flex: Ekran hangsini kaç pay alacağını ayarlar
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextButton(
+                  onPressed: () {
+                    print("Tatlı Butonu Tıklandı");
+                  },
+                  child: Image.asset("assets/tatli_1.jpg")),
+            ),
+          ),
         ],
       ),
     );
