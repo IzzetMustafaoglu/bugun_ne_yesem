07b656a8131fc1ee10837fc5af6d88c9aa98a3f9
IzzetMustafaoglu <izzetmustafaoglu00@gmail.com>
2023-08-27 07:56:57 +0300

    İlk commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BenimUyg());
+}
+
+class BenimUyg extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            "Bugün Ne Yesem?",
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
+        body: YemekSayfasi(),
+      ),
+    );
+  }
+}
+class YemekSayfasi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Image.asset("assets/corba_1.jpg"),
+          Image.asset("assets/yemek_1.jpg"),
+          Image.asset("assets/tatli_1.jpg")
+        ],
+      ),
+    );
+  }
+}
